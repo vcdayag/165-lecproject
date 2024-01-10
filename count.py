d52a847352bbfce50d2d3b5b0f4e214b09a1e6d2
Van Paul Dayag <dayag.vanpaul@gmail.com>
2024-01-10 10:02:32 +0800

    feat: use counting template

---
+++ count.py
@@ -0,0 +1,40 @@
+# https://docs.ultralytics.com/guides/object-counting/#real-world-applications
+from ultralytics import YOLO
+from ultralytics.solutions import object_counter
+import cv2
+
+model = YOLO("yolov8n.pt")
+cap = cv2.VideoCapture("data/VID_20231220_132423.mp4")
+assert cap.isOpened(), "Error reading video file"
+w, h, fps = (int(cap.get(x)) for x in (cv2.CAP_PROP_FRAME_WIDTH, cv2.CAP_PROP_FRAME_HEIGHT, cv2.CAP_PROP_FPS))
+
+region_points = [(500, 0), (550, 0),(550, 1080),(500, 1080)]  # line or region points
+classes_to_count = [2, 7]  # car and truck classes for count
+
+# Video writer
+video_writer = cv2.VideoWriter("object_counting_output.avi",
+                       cv2.VideoWriter_fourcc(*'mp4v'),
+                       fps,
+                       (w, h))
+
+# Init Object Counter
+counter = object_counter.ObjectCounter()
+counter.set_args(view_img=True,
+                 reg_pts=region_points,
+                 classes_names=model.names,
+                 draw_tracks=True)
+
+while True and cap.isOpened():
+    success, im0 = cap.read()
+    if not success:
+        print("Video frame is empty or video processing has been successfully completed.")
+        break
+    tracks = model.track(im0, persist=True, show=False,
+                         classes=classes_to_count)
+
+    im0 = counter.start_counting(im0, tracks)
+    video_writer.write(im0)
+
+cap.release()
+video_writer.release()
+cv2.destroyAllWindows()
