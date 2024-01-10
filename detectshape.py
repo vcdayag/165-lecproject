5be6c38cbcb59ac2c3417a6b17cf1bfb57366b0d
Van Paul Dayag <dayag.vanpaul@gmail.com>
2024-01-10 18:03:57 +0800

    feat: check contours for red and yellow

--- detectshape.py
+++ detectshape.py
@@ -1,13 +1,15 @@
 import cv2
+from stickerdetection import checkColor
 
+AREA_MIN = 2000
+AREA_MAX = 5000
+MIN_YELLOW = 10
+MIN_RED = 10
 
 def detectShape(img: cv2.typing.MatLike, mask: cv2.typing.MatLike):
-    # reading image
-    # img = cv2.imread('data/IMG_20231220_132420.jpg')
-
     # converting image into grayscale image
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    countourmask = mask.copy()
+    contourmask = mask.copy()
 
     # setting threshold of gray image
     _, threshold = cv2.threshold(gray, 127, 255, cv2.THRESH_BINARY)
@@ -18,10 +20,7 @@ def detectShape(img: cv2.typing.MatLike, mask: cv2.typing.MatLike):
     # here we are ignoring first counter because
     # findcontour function detects whole image as shape
     contours = contours[1:]
-
-    AREA_MIN = 2000
-    AREA_MAX = 5000
-
+    
     contourBoundingBox = []
 
     # list for storing names of shapes
@@ -33,56 +32,30 @@ def detectShape(img: cv2.typing.MatLike, mask: cv2.typing.MatLike):
             continue
 
         contourBoundingBox.append((x, y, w, h))
-        # cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255,0), 2)
-
-        # cv2.approxPloyDP() function to approximate the shape
-        # approx = cv2.approxPolyDP(
-        #     contour, 0.01 * cv2.arcLength(contour, True), True)
-
-        # using drawContours() function
-        # cv2.drawContours(img, [contour], 0, (0, 0, 255), 5)
-        cv2.drawContours(countourmask, [contour], -1, (0, 0, 0), -1)
-
-        # finding center point of shape
-        # M = cv2.moments(contour)
-        # x = 0
-        # y = 0
-        # if M['m00'] != 0.0:
-        #     x = int(M['m10']/M['m00'])
-        #     y = int(M['m01']/M['m00'])
-
-        # putting shape name at center of each shape
-        # if len(approx) == 4:
-        #     cv2.putText(img, 'Quadrilateral', (x, y),
-        #                 cv2.FONT_HERSHEY_SIMPLEX, 0.6, (255, 255, 255), 2)
-        # if len(approx) == 3:
-        #     cv2.putText(img, 'Triangle', (x, y),
-        #                 cv2.FONT_HERSHEY_SIMPLEX, 0.6, (255, 255, 255), 2)
-
-        # elif len(approx) == 4:
-        #     cv2.putText(img, 'Quadrilateral', (x, y),
-        #                 cv2.FONT_HERSHEY_SIMPLEX, 0.6, (255, 255, 255), 2)
-
-        # elif len(approx) == 5:
-        #     cv2.putText(img, 'Pentagon', (x, y),
-        #                 cv2.FONT_HERSHEY_SIMPLEX, 0.6, (255, 255, 255), 2)
-
-        # elif len(approx) == 6:
-        #     cv2.putText(img, 'Hexagon', (x, y),
-        #                 cv2.FONT_HERSHEY_SIMPLEX, 0.6, (255, 255, 255), 2)
-
-        # else:
-        #     cv2.putText(img, 'circle', (x, y),
-        #                 cv2.FONT_HERSHEY_SIMPLEX, 0.6, (255, 255, 255), 2)
+        cv2.drawContours(contourmask, [contour], -1, (0, 0, 0), -1)
 
     # displaying the image after drawing contours
-    finalmask = cv2.bitwise_xor(countourmask, mask)
+    finalmask = cv2.bitwise_xor(contourmask, mask)
     finalimage = cv2.bitwise_and(img, img, mask=finalmask)
 
     for c in contourBoundingBox:
-        finalimage = cv2.rectangle(
-            finalimage, (c[0], c[1]), (c[0] + c[2], c[1] + c[3]), (0, 255, 0), 2
-        )
+        X,Y,W,H = c
+            
+        croppedimage = finalimage[Y:Y+H,X:X+W]
+        red_output = checkColor(croppedimage,"red1")
+        red_pixels = cv2.countNonZero(red_output)
+        
+        yellow_output = checkColor(croppedimage,"yellow2")
+        yellow_pixels = cv2.countNonZero(yellow_output)
+        
+        print("   red:",red_pixels)
+        print("yellow:",yellow_pixels)
+        print()
+        
+        if red_pixels > MIN_RED and yellow_pixels > MIN_YELLOW:
+            finalimage = cv2.rectangle(
+                finalimage, (c[0], c[1]), (c[0] + c[2], c[1] + c[3]), (0, 255, 0), 2
+            )
 
     cv2.imshow("shapes", finalimage)
     cv2.waitKey(0)
