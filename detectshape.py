3cb2fcc35770399a630b2c4594e97b83ca582300
Van Paul Dayag <dayag.vanpaul@gmail.com>
2024-01-10 23:44:48 +0800

    feat: check ratios

--- detectshape.py
+++ detectshape.py
@@ -5,18 +5,16 @@ AREA_MIN = 2000
 AREA_MAX = 5000000
 MIN_YELLOW = 50
 MIN_RED = 50
+PIXELS_AREA_RATIO = 0.35
+RED_YELLOW_RATIO_MIN = 0.10
+RED_YELLOW_RATIO_MAX = 5
 
 
 def detectShape(img: cv2.typing.MatLike, mask: cv2.typing.MatLike, car_number: int):
-  
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) # convert from RGB to grayscale
-    contour_mask = mask.copy() # saves a copy of the mask
+    contour_mask = mask.copy()  # saves a copy of the mask
 
-    # setting threshold of gray image
-    _, threshold = cv2.threshold(gray, 127, 255, cv2.THRESH_BINARY)
-
-    # find the contours in the thresholded image
-    contours, _ = cv2.findContours(threshold, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    # find the contours in the mask image
+    contours, _ = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
     # here we are ignoring first counter because
     # findcontour function detects whole image as shape
@@ -26,7 +24,7 @@ def detectShape(img: cv2.typing.MatLike, mask: cv2.typing.MatLike, car_number: i
 
     # list for storing names of shapes
     for c in contours:
-        x, y, w, h = cv2.boundingRect(c) # extracting the coordinates
+        x, y, w, h = cv2.boundingRect(c)  # extracting the coordinates
         contour_area = w * h
 
         # filters contours too small or too large
@@ -52,28 +50,47 @@ def detectShape(img: cv2.typing.MatLike, mask: cv2.typing.MatLike, car_number: i
 
         # returns a binary mask where yellow pixels are white and the rest are black
         yellow_output = checkColor(croppedimage, "yellow2")
-        yellow_pixels = cv2.countNonZero(yellow_output) # counts amount of yellow pixels
-
-       
+        yellow_pixels = cv2.countNonZero(
+            yellow_output
+        )  # counts amount of yellow pixels
 
         # returns a binary mask where red pixels are white and the rest are black
         red_output = checkColor(croppedimage, "red1")
-        red_pixels = cv2.countNonZero(red_output) # counts amount of red pixels
+        red_pixels = cv2.countNonZero(red_output)  # counts amount of red pixels
 
         # filters out contours that don't have enough red or yellow
         if yellow_pixels < MIN_YELLOW or red_pixels < MIN_RED:
             continue
 
-        # bounds the detected sticker
-        final_image = cv2.rectangle(
-            final_image, (c[0], c[1]), (c[0] + c[2], c[1] + c[3]), (0, 255, 0), 2
-        )
-        possibleStickers.append(c)
+        totalpixels = red_pixels + yellow_pixels
+        area = W * H
+        red_yellow_ratio = yellow_pixels / red_pixels
 
         if True:
-            print("   red:", red_pixels)
-            print("yellow:", yellow_pixels)
+            print("        red:", red_pixels)
+            print("     yellow:", yellow_pixels)
+            print("totalpixels:", totalpixels)
+            print("       area:", area)
+            print("pix-A_ratio:", totalpixels / area)
+            print("   RY_ratio:", red_yellow_ratio)
             print()
+
+        # check if the number of pixel is less than 45% of area
+        if totalpixels < area * PIXELS_AREA_RATIO:
+            continue
+
+        # check if the red and yellow pixel ration
+        if (
+            red_yellow_ratio < RED_YELLOW_RATIO_MIN
+            or red_yellow_ratio > RED_YELLOW_RATIO_MAX
+        ):
+            continue
+
+        # bounds the detected sticker
+        finalimage = cv2.rectangle(
+            finalimage, (c[0], c[1]), (c[0] + c[2], c[1] + c[3]), (0, 255, 0), 2
+        )
+        possibleStickers.append(c)
 
         cv2.imwrite(
             f"output/car_{car_number}_{red_pixels}_{yellow_pixels}.jpg", croppedimage
