ad6dd1f1eef8b7e048ce095cff933241ea4b8027
Van Paul Dayag <dayag.vanpaul@gmail.com>
2024-01-10 16:08:40 +0800

    feat: use other function in count.py

--- count.py
+++ count.py
@@ -2,13 +2,15 @@
 from ultralytics import YOLO
 from ultralytics.solutions import object_counter
 import cv2
+from stickerdetection import StickerMask, StickerResult
+from detectshape import detectShape
 
 model = YOLO("yolov8n.pt")
 cap = cv2.VideoCapture("data/VID_20231220_132423.mp4")
 assert cap.isOpened(), "Error reading video file"
 w, h, fps = (int(cap.get(x)) for x in (cv2.CAP_PROP_FRAME_WIDTH, cv2.CAP_PROP_FRAME_HEIGHT, cv2.CAP_PROP_FPS))
 
-region_points = [(500, 0), (550, 0),(550, 1080),(500, 1080)]  # line or region points
+region_points = [(750, 0), (800, 0),(800, 1080),(750, 1080)]  # line or region points
 classes_to_count = [2, 7]  # car and truck classes for count
 
 # Video writer
@@ -24,25 +26,43 @@ counter.set_args(view_img=False,
                  classes_names=model.names,
                  draw_tracks=True)
 
+carcounter = 0
+trackedcars = []
 while True and cap.isOpened():
     success, im0 = cap.read()
-    if not success:
-        print("Video frame is empty or video processing has been successfully completed.")
-        break
+    
     tracks = model.track(im0, persist=True, show=False,
                          classes=classes_to_count)
 
-    im0 = counter.start_counting(im0, tracks)
-    print(counter.in_counts)
-    print(counter.out_counts)
-
-    cv2.imshow("ultralytics", im0)
+    counted_image = counter.start_counting(im0, tracks)
+    
+    if counted_image is None:
+        print("Video frame is empty or video processing has been successfully completed.")
+        break
+    # print(counter.in_counts)
+    # print(counter.out_counts)
+    if carcounter < counter.out_counts:
+        carcounter += 1
+        
+        mask = StickerMask(im0)
+        result = StickerResult(im0,mask)
+        
+        print(tracks[0].boxes)
+        trackedcars.append(tracks[0].boxes)
+        
+        detectShape(result)
+        # cv2.imshow("STICKER", result)
+        # cv2.waitKey(0) 
+        
+    # print(im0)
+    cv2.imshow("ultralytics", counted_image)
     # create output file
     # video_writer.write(im0)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
+print(trackedcars)
 cap.release()
 video_writer.release()
 cv2.destroyAllWindows()
